ff6ac2a73fb62a95085664d4c5b8fa29d30e84a5
Foylaou <s225002731@gmail.com>
2025-02-19 22:17:56 +0800

    Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,8 @@ interface EnvironmentConfig {
 }
 
 const getEnvironmentConfig = (): EnvironmentConfig => {
-  const API_URL = process.env.API || "https://ishabackend:8080";
-  const RAG_API = process.env.RAG_API || "https://ishabackend:8080";
+  const API_URL = process.env.API || "http://ishabackend:8080";
+  const RAG_API = process.env.RAG_API || "http://ishabackend:8080";
   const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3000";
   const NODE_ENV = process.env.NODE_ENV || "development";
   const isDev = NODE_ENV === "development";
